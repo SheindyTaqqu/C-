94370c2f6a7cd39eaf948d926ae82bd3ebb26f32
SheindyTaqqu <Hand1e13@mbjcomp.org.il>
2025-10-20 20:07:31 +0300

    Add files via upload

---
+++ HWLesson3/ConsoleApp3/Book2.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class Book : Item
+{
+    public string Author { get; set; }
+    public int Pages { get; set; }
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+    public double Weight { get; set; }
+    public double Price { get; set; }
+
+    public Book(int id, string title, string author, int pages, string genre, string publisher, double weight, double price)
+        : base(id, title)
+    {
+        Author = author;
+        Pages = pages;
+        Genre = genre;
+        Publisher = publisher;
+        Weight = weight;
+        Price = price;
+
+    }
+
+    public override string ToString()
+    {
+        return $"Book: {Title} by {Author}, {Pages} pages, Genre: {Genre}, Publisher: {Publisher},Weight:{Weight},Price:{Price}";
+    }
+}
---
+++ HWLesson3/ConsoleApp3/MyFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    public class MyFile
+    {
+        public MyFile() { }
+
+        public void openFile(string filename)
+        {
+
+            string FILENAME = "..\\..\\files\\" + filename; // השתמש בפרמטר filename
+            if (!File.Exists(FILENAME))
+                throw new FileNotFoundException("not found", FILENAME);
+            using (StreamWriter streamWriter = new StreamWriter(FILENAME, true))
+
+            {
+                streamWriter.WriteLine("1, world war 2, y.k, 500, children, Bloomsbury, 12, 70");
+                streamWriter.WriteLine("2, 1945, g.o, 328, Dystopian, yefey nof, 8, 85");
+                streamWriter.WriteLine("3, small world, shalom cohen, 200, Drama, yefey nof, 10, 100");
+            }
+           
+
+
+        }
+        public void readFile(string filename) // הוסף פרמטר filename
+        {
+            string FILENAME = "..\\..\\files\\" + filename; // הגדרת משתנה FILENAME
+            if (!File.Exists(FILENAME))
+            {
+                throw new FileNotFoundException("File not found", FILENAME);
+            }
+
+            using (StreamReader streamReader = new StreamReader(FILENAME))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line); // מדפיס את השורות לקונסול
+                }
+            }
+        }
+    }
+    }
